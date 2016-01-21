9c648ba5f91803cc7acd4079bb6016c1eb2336a5
Jakub Głuszecki <jakub.gluszecki@gmail.com>
2016-01-21 15:31:45 +0100

    rspec

---
+++ spec/base_image_spec.rb
@@ -0,0 +1,22 @@
+require "spec_helper"
+
+describe "Dockerfile" do
+  before(:all) do
+    image = Docker::Image.build_from_dir('.')
+    set :docker_image, image.id
+  end
+
+  describe package("sudo") do
+    it { should be_installed }
+  end
+
+  describe user('yeoman') do
+    it { should exist }
+    it { should have_home_directory '/home/yeoman' }
+  end
+
+  
+
+
+end
+
